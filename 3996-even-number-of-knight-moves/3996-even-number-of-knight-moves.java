cf2fcdadbc786c3dbe585b6ca5acd322cd11cd14
iaamirRashid <aamironlyaamir@gmail.com>
2026-07-20 01:34:48 +0530

    Time: 1 ms (0%), Space: 44 MB (0%) - LeetHub

---
+++ 3996-even-number-of-knight-moves/3996-even-number-of-knight-moves.java
@@ -0,0 +1,8 @@
+class Solution {
+    public boolean reachCan(int[] start, int[] target) {
+return (start[0] + start[1]) % 2 == (target[0] + target[1]) % 2;
+    }
+    public boolean canReach(int[] start, int[] target) {
+        return reachCan(start, target);
+    }
+}
