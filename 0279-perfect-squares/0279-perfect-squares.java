91c4c17376b702ecef0ad3fdaa949812664ef11f
iaamirRashid <aamironlyaamir@gmail.com>
2026-07-28 12:55:11 +0530

    Time: 31 ms (69.58%), Space: 44.4 MB (76.93%) - LeetHub

---
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int squareNum(int n) {
+      
+      int dp[] = new int[n+1];
+
+      dp[0] = 0;
+
+      for(int i=1; i<=n; i++) {
+        dp[i] = Integer.MAX_VALUE;
+        for(int j=1; j*j<=i; j++) {
+         dp[i] = Math.min(dp[i], 1 + dp[i - j*j]);
+        }
+      }
+      return dp[n];
+    }
+    public int numSquares(int n) {
+        return squareNum(n);
+    }
+}
