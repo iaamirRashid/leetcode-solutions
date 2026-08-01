48c453b4d5f13e45e0b0487e447ac5bad6054511
iaamirRashid <aamironlyaamir@gmail.com>
2026-08-01 11:27:36 +0530

    Time: 1 ms (51.87%), Space: 43.1 MB (10.58%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean winnerThePredict(int[] nums) {
+       int n = nums.length;
+
+       int dp[][] = new int[n][n];
+
+       for(int i=0; i<n; i++) {
+        dp[i][i] = nums[i];
+       }
+
+       for(int len=2; len<=n; len++) {
+        for(int i=0; i<=n-len; i++) {
+            int j = i + len - 1;
+
+            int pickLeft = nums[i] - dp[i+1][j];
+            int pickRight = nums[j] - dp[i][j-1];
+
+            dp[i][j] = Math.max(pickLeft, pickRight);
+        }
+       }
+       return dp[0][n-1] >= 0;
+    }
+    public boolean predictTheWinner(int[] nums) {
+        return winnerThePredict(nums);
+    }
+}
