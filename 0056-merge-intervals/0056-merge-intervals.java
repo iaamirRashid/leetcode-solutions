bf4079fa0e0b786cf43a5ec9ab55f021d88ea690
iaamirRashid <aamironlyaamir@gmail.com>
2026-08-04 13:04:20 +0530

    Time: 8 ms (91.47%), Space: 49 MB (75.83%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,20 +1,26 @@
 class Solution {
     public static int[][] Merge(int[][] intervals) {
+// Step 1: Sort intervals by starting time
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
 
-        Arrays.sort(intervals, (a,b)-> Integer.compare(a[0], b[0]));
+        List<int[]> ans = new ArrayList<>();
+        // Step 2: Add first interval
+        ans.add(intervals[0]);
+        // Step 3: Traverse remaining intervals
+        for (int i = 1; i < intervals.length; i++) {
 
-        List<int[]> result = new ArrayList<>();
-
-        for(int[] interval : intervals) {
-        if(result.isEmpty() || result.get(result.size()-1)[1] < interval[0]) {
-            result.add(interval);
-        } else {
-         result.get(result.size()-1)[1] = Math.max(result.get(result.size()-1)[1], interval[1]);
+            int[] last = ans.get(ans.size() - 1);
+            // Overlap
+            if (intervals[i][0] <= last[1]) {
+                last[1] = Math.max(last[1], intervals[i][1]);
+            }
+            // No overlap
+            else {
+                ans.add(intervals[i]);
+            }
         }
-        }
 
-        return result.toArray(new int[result.size()][]);
-
+        return ans.toArray(new int[ans.size()][]);
     }
     public int[][] merge(int[][] intervals) {
         return Merge(intervals);
